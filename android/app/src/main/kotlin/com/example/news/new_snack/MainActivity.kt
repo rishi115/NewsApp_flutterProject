e62f3beeff60ab769b2dd83cef5a0189ff0f3b58
rishikesh devare <rishidevare051@gmail.com>
2023-04-05 00:13:27 +0530

    code done

---
+++ android/app/src/main/kotlin/com/example/news/new_snack/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.news.new_snack
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:new_snack/view/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'News Snack',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+
